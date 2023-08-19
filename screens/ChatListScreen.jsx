b0bd3f6a19c6788cfe8f059232950c2c7d76bd7f
Moe Misri <mohammed.masry74@gmail.com>
2023-08-19 19:08:19 +0300

    set-up

---
+++ screens/ChatListScreen.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const ChatListScreen = props => {
+    
+    return (
+        <View style={styles.container}>
+            <Text>Chat list screen</Text>
+            <Button title='Go to settings' onPress={()=> props.navigation.navigate("Settings")} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default ChatListScreen;
