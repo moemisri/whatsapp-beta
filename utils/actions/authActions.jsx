6bd931bfbcad0764c6c71676b93e9db0c0e4ea14
Moe Misri <mohammed.masry74@gmail.com>
2023-08-22 14:55:33 +0300

    creating user in the realtime database 🥳🥳🥳🥳

--- utils/actions/authActions.jsx
+++ utils/actions/authActions.jsx
@@ -1,12 +1,45 @@
 import { getFirebaseApp } from "../firebaseHelper";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { child, getDatabase, ref, set } from "firebase/database";
+
 export const signUp = async (firstName, lastName, email, password) => {
   const app = getFirebaseApp();
   const auth = getAuth(app);
+
   try {
     const result = await createUserWithEmailAndPassword(auth, email, password);
-    console.log("result", result);
+    const { uid } = result.user;
+
+    const userData = await createUser(firstName, lastName, email, uid);
+
+    console.log(userData);
   } catch (error) {
-    console.error(error.code);
+    console.log(error);
+    const errorCode = error.code;
+
+    let message = "Something went wrong.";
+
+    if (errorCode === "auth/email-already-in-use") {
+      message = "This email is already in use";
+    }
+
+    throw new Error(message);
   }
+};
+
+const createUser = async (firstName, lastName, email, userId) => {
+  const firstLast = `${firstName} ${lastName}`.toLowerCase();
+  const userData = {
+    firstName,
+    lastName,
+    firstLast,
+    email,
+    userId,
+    signUpDate: new Date().toISOString(),
+  };
+
+  const dbRef = ref(getDatabase());
+  const childRef = child(dbRef, `users/${userId}`);
+  await set(childRef, userData);
+  return userData;
 };
