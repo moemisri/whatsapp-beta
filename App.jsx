07f00160f8f9e024ab1621a6126beebfd2ac14db
moenergetic <profmisri@gmail.com>
2024-05-05 01:52:29 +0300

    all done

--- App.jsx
+++ App.jsx
@@ -7,9 +7,11 @@ import * as Font from "expo-font";
 import AppNavigator from "./navigation/AppNavigator";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { MenuProvider } from "react-native-popup-menu";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 LogBox.ignoreLogs(["AsyncStorage has been extracted"]);
+// AsyncStorage.clear();
 
 SplashScreen.preventAutoHideAsync();
 
@@ -21,18 +23,18 @@ export default function App() {
       try {
         // prettier-ignore
         await Font.loadAsync({
-          black: require("./assets/fonts//Roboto-Black.ttf"),
-          blackItalic: require("./assets/fonts/Roboto-BlackItalic.ttf"),
-          bold: require("./assets/fonts/Roboto-Bold.ttf"),
-          boldItalic: require("./assets/fonts/Roboto-BoldItalic.ttf"),
-          italic: require("./assets/fonts/Roboto-Italic.ttf"),
-          light: require("./assets/fonts/Roboto-Light.ttf"),
-          lightItalic: require("./assets/fonts/Roboto-LightItalic.ttf"),
-          medium: require("./assets/fonts/Roboto-Medium.ttf"),
-          mediumItalic: require("./assets/fonts/Roboto-MediumItalic.ttf"),
-          regular: require("./assets/fonts/Roboto-Regular.ttf"),
-          thin: require("./assets/fonts/Roboto-Thin.ttf"),
-          thinItalic: require("./assets/fonts/Roboto-ThinItalic.ttf"),
+          "black": require("./assets/fonts//Roboto-Black.ttf"),
+          "blackItalic": require("./assets/fonts/Roboto-BlackItalic.ttf"),
+          "bold": require("./assets/fonts/Roboto-Bold.ttf"),
+          "boldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
+          "italic": require("./assets/fonts/Roboto-Italic.ttf"),
+          "light": require("./assets/fonts/Roboto-Light.ttf"),
+          "lightItalic": require("./assets/fonts/Roboto-LightItalic.ttf"),
+          "medium": require("./assets/fonts/Roboto-Medium.ttf"),
+          "mediumItalic": require("./assets/fonts/Roboto-MediumItalic.ttf"),
+          "regular": require("./assets/fonts/Roboto-Regular.ttf"),
+          "thin": require("./assets/fonts/Roboto-Thin.ttf"),
+          "thinItalic": require("./assets/fonts/Roboto-ThinItalic.ttf"),
         });
       } catch (error) {
         console.log.error();
@@ -57,7 +59,9 @@ export default function App() {
   return (
     <Provider store={store}>
       <SafeAreaProvider style={styles.container} onLayout={onLayout}>
-        <AppNavigator />
+        <MenuProvider>
+          <AppNavigator />
+        </MenuProvider>
       </SafeAreaProvider>
     </Provider>
   );
